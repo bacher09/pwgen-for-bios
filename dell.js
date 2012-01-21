ce4d3d5cc78bb29cf7db2c5771cdfd8fd31056ee
Slava Bacherikov <slava@bacher09.org>
2012-01-21 21:33:58 +0200

    Add MD5magic array.

--- dell.js
+++ dell.js
@@ -10,6 +10,26 @@ var  scancods = "\00\0331234567890-=\010\011qwertyuiop[]\015\377asdfghjkl;'`\377
 var encData = [0x1,0x23,0x45,0x67,0x89,0xAB,0xCD,0xEF,0xFE,0xDC,0xBA,0x98,
                0x76, 0x54, 0x32, 0x10];
 
+var MD5magic = [
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x4881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+            ];
+
+
 function ord(str){
     return str.charCodeAt(0);
 }
@@ -147,15 +167,20 @@ function calc_suffix_hdd_old(serial){
 
 
 function blockEncode(){
+    var m_encData  = copy_array(encData);
+    var A = m_encData[0];
+    var B = m_encData[1];
+    var C = m_encData[2];
+    var D = m_encData[3];
+
 
 
 }
 
 function encode(in_str, cnt){
-    var m_encData  = copy_array(encData);
-    in_str[cnt + 1] = 0x80;
+    in_str[cnt] = 0x80;
     var encBlock = StringToIntArr(in_str);
-    encBlock = fill_zero(encBlock,10,16);
+    encBlock = fill_zero(encBlock,6,16);
     encBlock[14] = (cnt << 3); 
 
 
