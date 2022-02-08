e745165cbba35cacec1babc3981321c451524b39
Slava Bacherikov <slava@bacher09.org>
2022-02-08 21:09:44 +0200

    Update unit tests for hpami add few extra cases

--- src/keygen/hpami.spec.ts
+++ src/keygen/hpami.spec.ts
@@ -5,6 +5,9 @@ describe("HP AMI BIOS", () => {
         expect(hpAMISolver("A7AF422F")).toEqual(["49163252"]);
         expect(hpAMISolver("12345678")).toEqual(["2ae211b4"]);
         expect(hpAMISolver("48A02676")).toEqual(["27545092"]);
+        expect(hpAMISolver("B60BD282")).toEqual(["489b5bf9"]);
+        expect(hpAMISolver("757EDC82")).toEqual(["edfe2edd"]);
+        expect(hpAMISolver("7d94422f")).toEqual(["e4eea2c4"]);
     });
     it("Check crc32", () => {
         let crc = new Crc32();
